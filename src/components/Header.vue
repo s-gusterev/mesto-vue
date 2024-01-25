bd04578555c3523c5619b6d38e94c6a63feb901b
Sergey Gusterev <copywriter-sg@yandex.ru>
2024-01-25 15:09:46 +0300

    feat: template header

---
+++ src/components/Header.vue
@@ -0,0 +1,11 @@
+<script setup>
+import logo from "../images/logo.svg";
+</script>
+
+<template>
+  <header className="header root__header">
+    <a href="/" className="logo header__logo"
+      ><img :src="logo" alt="Логотип" className="logo__img"
+    /></a>
+  </header>
+</template>
